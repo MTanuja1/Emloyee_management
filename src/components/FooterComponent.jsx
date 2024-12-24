26e19287e9a79ad0876594f93fbc2b6fd7517780
MTanuja1 <tanujamehra2002@gmail.com>
2024-12-24 21:02:32 +0530

    initial commit

---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span>FullStack project using springboot and react</span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
